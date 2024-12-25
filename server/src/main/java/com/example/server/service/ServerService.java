34bdf28d29c09913e1c3f6eaabe1db5900a19ae9
KTsybulka <ktsybulka11@gmail.com>
2024-12-24 20:42:09 -0400

    feat: implement server management methods

--- server/src/main/java/com/example/server/service/ServerService.java
+++ server/src/main/java/com/example/server/service/ServerService.java
@@ -2,12 +2,14 @@ package com.example.server.service;
 
 import com.example.server.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.List;
 
 public interface ServerService {
-    Server create(Server server);
-    Server pingServer(Server server);
+    Server createServer(Server server);
+    Server pingServer(String ipAdress) throws IOException;
     Collection<Server> getAllSercers(int limit);
     Server getServerById(Long id);
     Server updateServer(Server server);
--- server/src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
+++ server/src/main/java/com/example/server/service/implementation/ServerServiceImpl.java
@@ -1,37 +1,65 @@
 package com.example.server.service.implementation;
 
+import com.example.server.enumeration.Status;
 import com.example.server.model.Server;
 import com.example.server.repository.ServerRepo;
 import com.example.server.service.ServerService;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.util.Collection;
 import java.util.List;
 
+import static com.example.server.enumeration.Status.SERVER_DOWN;
+import static com.example.server.enumeration.Status.SERVER_UP;
+
+//in Lombok automatically generates a constructor for all the final fields
+// and any fields annotated with @NonNull in a class.
+@RequiredArgsConstructor
+@Service
+@Transactional
+@Slf4j
 public class ServerServiceImpl implements ServerService {
     private final ServerRepo serverRepo;
 
-    public ServerServiceImpl(ServerRepo serverRepo) {
-        this.serverRepo = serverRepo;
-    }
 
     @Override
-    public Server create(Server server) {
+    public Server createServer(Server server) {
+        log.info("Creating server with name: {}", server.getName());
+        server.setImageURL(setServerImageURL());
+        return serverRepo.save(server);
+    }
+
+    private String setServerImageURL() {
         return null;
     }
 
     @Override
-    public Server pingServer(Server server) {
-        return null;
+    public Server pingServer(String ipAdress) throws IOException {
+        log.info("Pinging server IP: {}", ipAdress);
+        Server server =  serverRepo.findByIpAdress(ipAdress);
+        InetAddress address = InetAddress.getByName(ipAdress);
+        server.setStatus(address.isReachable(10000) ? SERVER_UP : SERVER_DOWN);
+        serverRepo.save(server);
+        return server;
     }
 
     @Override
     public Collection<Server> getAllSercers(int limit) {
-        return List.of();
+        log.info("Fetching all servers");
+        return serverRepo.findAll(PageRequest.of(0, limit)).toList();
     }
 
     @Override
     public Server getServerById(Long id) {
-        return null;
+        log.info("Fetching server by id: {}", id);
+        return serverRepo.findById(id).orElseThrow(() -> new RuntimeException("Server not found with id"));
     }
 
     @Override
