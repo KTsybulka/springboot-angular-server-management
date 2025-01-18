9b5635b332adb030c3395336cd12154cb8d90078
KTsybulka <ktsybulka11@gmail.com>
2025-01-17 21:51:16 -0400

    Add ServerService for fetching server list with HTTP client in Angular

--- client-side/src/app/service/server.service.ts
+++ client-side/src/app/service/server.service.ts
@@ -1,8 +1,15 @@
+import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { CustomResponse } from '../interfac/custom-response';
+import { Observable } from 'rxjs';
 
 @Injectable({  providedIn: 'root'})
 
 export class ServerService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getServer() : Observable<CustomResponse>{
+    return this.http.get<CustomResponse>('http://localhost:8080/server/list');
+  }
 }
