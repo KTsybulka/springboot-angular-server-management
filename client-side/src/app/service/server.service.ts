f0456f7c39bcbfa7aaeb46d3fa6e43a321d3a33b
KTsybulka <ktsybulka11@gmail.com>
2025-01-26 19:06:49 -0500

    Enhancing Readability and Robustness in Observable-Based HTTP Operations

--- client-side/src/app/service/server.service.ts
+++ client-side/src/app/service/server.service.ts
@@ -1,15 +1,26 @@
 import { HttpBackend, HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CustomResponse } from '../interfac/custom-response';
-import { Observable } from 'rxjs';
+import { catchError, Observable, tap, throwError } from 'rxjs';
 
 @Injectable({  providedIn: 'root'})
 
 export class ServerService {
 
+
+  private readonly apiUrl = 'any';
+
   constructor(private http: HttpClient) { }
 
-  getServer() : Observable<CustomResponse>{
-    return this.http.get<CustomResponse>('http://localhost:8080/server/list');
+  severs$ = <Observable<CustomResponse>>
+  this.http.get<CustomResponse>(`${this.apiUrl}/server/list`)
+  .pipe(
+    tap(console.log),
+    catchError(this.handlerError)
+  );
+
+
+  handlerError(error: any): Observable<never> {
+    return throwError ('Method not implemented.');
   }
 }
